a63bb38d64994c5020b3b2231d503c7318081425
qrugha <phenriquealves191@gmail.com>
2026-09-10 16:55:50 -0300

    feat: conexão com banco de dados efetuada

---
+++ src/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import { pool } from "./database/db.js";
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get("/equipamento", async (req, res) => {
+  try {
+    const response = await pool.query("SELECT * FROM equipamento");
+    res.json(response.rows);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`API rodando em: http://localhost:3000`);
+});
